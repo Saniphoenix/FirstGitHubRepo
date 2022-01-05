653ce07766742d5a666e63b5676c9015516e14c0
Saniphoenix <97071746+Saniphoenix@users.noreply.github.com>
2022-01-04 19:52:39 -0500

    Update Git.java
    this is for practice

--- src/Git.java
+++ src/Git.java
@@ -11,5 +11,8 @@ public class Git {
 		system.out.println("this is changes from upstream, upstream means in remore repository");
 	
 	}
+	
+	public void methodB(){
+	// This is second method for practice purpose}
 
 }
