c5e935037df55259414973eafb9722133444b50e
Saniphoenix <97071746+Saniphoenix@users.noreply.github.com>
2022-01-05 16:42:50 -0500

    Update Git.java
    This is changes in repo.

--- src/Git.java
+++ src/Git.java
@@ -4,13 +4,11 @@ public class Git {
 	public static void main(String[] args) {
 		
 		
+	}
+	
+	public void methodA(){
+	System.out.println("This is changes from upstream, upstream means repository");
 		
-		System.out.println("this is first test");
-		
-		
-			
-			
-
 	}
 
 }
