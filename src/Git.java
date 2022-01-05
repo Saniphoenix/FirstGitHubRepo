f16a9aa486c8a9359660df74e1f7fdf2438a9c6f
Saniphoenix <97071746+Saniphoenix@users.noreply.github.com>
2022-01-04 19:37:17 -0500

    Update Git.java

--- src/Git.java
+++ src/Git.java
@@ -4,6 +4,12 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+	}
+	
+	public void methodA(){
+		system.out.println("this is changes from upstream, upstream means in remore repository");
+	
 	}
 
 }
